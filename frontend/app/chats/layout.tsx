3b4cdabe3466cb2e58c7adb603181672a19f5844
Nitesh <87512@arsd.du.ac.in>
2026-03-02 02:23:57 +0530

    fix: fix service bug

--- frontend/app/chats/layout.tsx
+++ frontend/app/chats/layout.tsx
@@ -1,29 +1,15 @@
-"use client";
 import { cookies } from "next/headers";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import ChatsClientLayout from "./clientLayout";
-import { apiFetch } from "@/lib/api";
-import { useEffect } from "react";
 
 export default async function ChatsLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  const cookieStore = await cookies();
-  const token = cookieStore.get("accessToken");
-  console.log(token);
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("accessToken");
+  // console.log(token)
 
-  useEffect(() => {
-    async function checkAuth() {
-      try {
-        // Try calling protected route
-        await apiFetch("/api/auth/me");
-      } catch {
-        // Not authenticated → stay on login/register
-        // If success → user is authenticated
-        router.replace("/login");
-      }
-    }
+  // if (!token) {
+  //   redirect("/login");
+  // }
 
-    checkAuth();
-  }, [router]);
   return <ChatsClientLayout>{children}</ChatsClientLayout>;
 }
