e7ed83c891438fb4f7956368c7cfafa44aa45e79
Nitesh <87512@arsd.du.ac.in>
2026-03-03 01:50:21 +0530

    fix: fix service bug

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,15 +1,6 @@
 import { LoginForm } from "@/components/login-form";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("accessToken");
-  console.log(token);
-
-  if (token) {
-    redirect("/chats");
-  }
   return (
     <div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 ">
       <div className="flex w-full max-w-sm flex-col gap-6">
--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,15 +1,6 @@
 import { SignupForm } from "@/components/signup-form";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default async function SignupPage() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("accessToken");
-  console.log(token);
-
-  if (token) {
-    redirect("/chats");
-  }
   return (
     <div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 ">
       <div className="flex w-full max-w-sm flex-col gap-6">
