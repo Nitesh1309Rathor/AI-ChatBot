df8d47f621964b90ec68bada07fa87a7787888a9
Nitesh <87512@arsd.du.ac.in>
2026-03-03 01:53:00 +0530

    fix: fix service bug

--- frontend/app/chats/layout.tsx
+++ frontend/app/chats/layout.tsx
@@ -1,15 +1,5 @@
-import { cookies } from "next/headers";
 import ChatsClientLayout from "./clientLayout";
-import { redirect } from "next/navigation";
 
 export default async function ChatsLayout({ children }: { children: React.ReactNode }) {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("accessToken");
-  console.log(token);
-
-  if (!token) {
-    redirect("/login");
-  }
-
   return <ChatsClientLayout>{children}</ChatsClientLayout>;
 }
